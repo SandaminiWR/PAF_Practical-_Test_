1c7067f3910f0731075266976cfaf03c2240665b
SandaminiWR <it19192710@my.sliit.lk>
2021-05-14 21:14:52 +0530

    Build the test proj

---
+++ UserManagement/WebContent/Components/user.js
@@ -0,0 +1,164 @@
+$(document).ready(function()
+{ 
+if ($("#alertSuccess").text().trim() == "") 
+ { 
+ $("#alertSuccess").hide(); 
+ } 
+ $("#alertError").hide(); 
+});
+
+// SAVE ============================================
+$(document).on("click", "#btnSave", function(event) 
+{ 
+// Clear alerts---------------------
+ $("#alertSuccess").text(""); 
+ $("#alertSuccess").hide(); 
+ $("#alertError").text(""); 
+ $("#alertError").hide(); 
+// Form validation-------------------
+var status = validateUserForm(); 
+if (status != true) 
+ { 
+ $("#alertError").text(status); 
+ $("#alertError").show(); 
+ return; 
+ } 
+// If valid-------------------------
+var type = ($("#hidUserIDSave").val() == "") ? "POST" : "PUT";
+ 
+ $.ajax( 
+ { 
+		
+ 	 url : "UsersAPI", 
+	 type : type, 
+ 	 data : $("#UsersAPI").serialize(), 
+	 dataType : "text", 
+  	 complete : function(response, status) 
+	 { 
+ 		onUserSaveComplete(response.responseText, status); 
+	 } 
+ 	}); 
+});
+
+function onUserSaveComplete(response, status)
+{ 
+	if (status == "success") 
+ 	{ 
+ 		var resultSet = JSON.parse(response); 
+	
+		 if (resultSet.status.trim() == "success") 
+		 { 
+			 $("#alertSuccess").text("Successfully saved."); 
+			 $("#alertSuccess").show(); 
+ 
+			$("#divUsersGrid").html(resultSet.data); 
+ 		
+		} else if (resultSet.status.trim() == "error") 
+ 		{ 
+ 			$("#alertError").text(resultSet.data); 
+		    $("#alertError").show(); 
+ 		} 
+	
+	 } else if (status == "error") 
+ 		{ 
+ 			$("#alertError").text("Error while saving."); 
+ 			$("#alertError").show(); 
+	 } else
+		 { 
+ 			$("#alertError").text("Unknown error while saving.."); 
+		    $("#alertError").show(); 
+ 		} 
+		    $("#hidUserIDSave").val(""); 
+ 			$("#formUser")[0].reset(); 
+};
+
+// UPDATE==========================================
+
+$(document).on("click", ".btnUpdate", function(event) 
+{ 
+	 $("#hidUserIDSave").val($(this).data("userid")); 
+	 $("#name").val($(this).closest("tr").find('td:eq(1)').text()); 
+	 $("#phone").val($(this).closest("tr").find('td:eq(2)').text()); 
+	 $("#address").val($(this).closest("tr").find('td:eq(3)').text()); 
+	 $("#mail").val($(this).closest("tr").find('td:eq(4)').text()); 
+	 $("#password").val($(this).closest("tr").find('td:eq(5)').text()); 
+	 $("#confirmpassword").val($(this).closest("tr").find('td:eq(6)').text()); 
+});
+
+// DELETE==========================================
+
+$(document).on("click", ".btnRemove", function(event)
+{ 
+ $.ajax( 
+ { 
+ url : "UsersAPI", 
+ type : "DELETE", 
+ data : "id=" + $(this).data("userid"),
+ dataType : "text", 
+ complete : function(response, status) 
+ { 
+ onUserDeleteComplete(response.responseText, status); 
+ } 
+ }); 
+});
+
+function onUserDeleteComplete(response, status)
+{ 
+	if (status == "success") 
+	 { 
+		 var resultSet = JSON.parse(response); 
+		 if (resultSet.status.trim() == "success") 
+	 { 
+		 $("#alertSuccess").text("Successfully deleted."); 
+		 $("#alertSuccess").show(); 
+		 $("#divUsersGrid").html(resultSet.data); 
+	 } else if (resultSet.status.trim() == "error") 
+	 { 
+		 $("#alertError").text(resultSet.data); 
+		 $("#alertError").show(); 
+	 } 
+	 } else if (status == "error") 
+	 { 
+		 $("#alertError").text("Error while deleting."); 
+		 $("#alertError").show(); 
+	 } else
+	 { 
+		 $("#alertError").text("Unknown error while deleting.."); 
+		 $("#alertError").show(); 
+	 } 
+}
+// CLIENT-MODEL================================================================
+function validateUserForm() 
+{ 
+// CODE
+if ($("#name").val().trim() == "") 
+ { 
+ return "Insert name."; 
+ } 
+// NAME
+if ($("#phone").val().trim() == "") 
+ { 
+ return "Insert Item Name."; 
+ } 
+// PRICE-------------------------------
+if ($("#address").val().trim() == "") 
+ { 
+ return "Insert Item Price."; 
+ } 
+if ($("#mail").val().trim() == "") 
+ { 
+ return "Insert Item Price."; 
+ } 
+if ($("#password").val().trim() == "") 
+ { 
+ return "Insert Item Price."; 
+ } 
+if ($("#confirmpasswprd").val().trim() == "") 
+ { 
+ return "Insert Item Price."; 
+ } 
+
+return true; 
+}
+
+
---
+++ UserManagement/WebContent/user.jsp
@@ -0,0 +1,72 @@
+<%@page import="com.User"%>
+<%@page import="com.DBConnection"%>
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+<meta charset="ISO-8859-1">
+<title>User Management</title>
+
+<link rel="stylesheet" href="Views/bootstrap.min.css">
+<script src="Components/jquery-3.2.1.min.js"></script>
+<script src="Components/user.js"></script>
+
+</head>
+<body>
+
+<div class="container"> 
+ <div class="row">
+ <div class="col-8"> 	
+	
+<h1 class="m-3">User Management</h1>
+	<form id="formUser" name= "formUser" >	
+ 
+Name : 
+<input id="name" name="name" type="text" 
+ class="form-control form-control-sm">
+<br>
+ Phone: 
+<input id="phone" name="phone" type="tel" 
+ class="form-control form-control-sm">
+<br> 
+Address : 
+<input id="address" name="address" type="text" 
+ class="form-control form-control-sm">
+<br>
+Email : 
+<input id="mail" name="mail" type="email" 
+ class="form-control form-control-sm">
+<br>
+Password : 
+<input id="password" name="password" type="password" 
+ class="form-control form-control-sm">
+<br>
+confirm Password: 
+<input id="confirmpassword" name="confirmpassword" type="password" 
+ class="form-control form-control-sm">
+<br>
+<input id="btnSave" name="btnSave" type="button" value="Save" 
+ class="btn btn-primary">
+<input type="hidden" id="hidUserIDSave" name="hidUserIDSave" value="">
+
+	</form>
+	
+<div id="alertSuccess" class="alert alert-success"></div>
+<div id="alertError" class="alert alert-danger"></div>
+
+	<br>
+	<div id="divUsersGrid">
+	<%
+		User userObj = new User(); 
+		 out.print(userObj.readUser()); 
+	%>
+</div>
+</div>
+	</div>
+ </div>
+ 
+</body>
+</html>
